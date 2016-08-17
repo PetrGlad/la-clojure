cbbf2f8bc350cd8b70d40ab7aca603b60884f2eb
Petr Gladkikh <petr.gladkikh@zalando.de>
2016-08-17 11:54:12 +0200

    Minor cleanup

--- src/clojure/org/jetbrains/plugins/clojure/init_clojure.clj
+++ src/clojure/org/jetbrains/plugins/clojure/init_clojure.clj
@@ -3,6 +3,5 @@
   (:require org.jetbrains.plugins.clojure.refactoring.clojure-refactoring-support-provider))
 
 (defn init []
-  (do
-    (org.jetbrains.plugins.clojure.intention.convert-import-intention/init)
-    (org.jetbrains.plugins.clojure.refactoring.clojure-refactoring-support-provider/init)))
+  (org.jetbrains.plugins.clojure.intention.convert-import-intention/init)
+  (org.jetbrains.plugins.clojure.refactoring.clojure-refactoring-support-provider/init))
