37e8556f07240143509cffc7849cb7e1cc3bfe30
Petr Gladkikh <petr.gladkikh@zalando.de>
2016-08-18 18:14:05 +0200

    Change parens' colors for better contrast with white background

--- src/java/org/jetbrains/plugins/clojure/editor/braceHighlighter/ClojureBraceAttributes.java
+++ src/java/org/jetbrains/plugins/clojure/editor/braceHighlighter/ClojureBraceAttributes.java
@@ -11,11 +11,11 @@ public abstract class ClojureBraceAttributes {
   private static final Color[] CLOJURE_BRACE_COLORS =
       {
           new Color(160, 160, 160), // gray
-          new Color(255, 160,   0), // orange
-          new Color(255, 255,   0), // yellow
-          new Color(160, 255,  62), // green
-          new Color( 32, 255, 224), // cyan
-          new Color( 64, 160, 255), // blue
+          new Color(255, 160, 0), // orange
+          new Color(200, 200, 0), // yellow
+          new Color(120, 200, 0), // green
+          new Color(0, 210, 180), // cyan
+          new Color(64, 160, 255), // blue
           new Color(160, 128, 255), // magenta
           new Color(224, 128, 192), // pink
       };
